0d223d852638399f4e2742b3cbbfa55147566a08
munyuawilson <wmunyua4@gmail.com>
2023-11-21 19:35:04 -0500

    navigation component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 require('./bootstrap');
+import Nav from './components/Nav'
+
+
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<Nav />,document.getElementById("nav"))
+
---
+++ resources/js/components/Nav.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+
+export default function Nav(){
+
+    return(
+        <div>
+            hello
+        </div>
+    )
+
+
+}
