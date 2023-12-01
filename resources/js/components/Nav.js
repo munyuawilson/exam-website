bef7c86e2c9677d3629d50292acb50e25b664105
munyuawilson <wmunyua4@gmail.com>
2023-12-01 10:50:51 -0500

    progress

--- resources/js/components/Nav.js
+++ resources/js/components/Nav.js
@@ -1,13 +1,12 @@
 
 import React from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+
 
 
 export default function Nav(){
 
     return(
-        <nav className="container-fluid navbar navbar-expand-md fixed-top mb-5 color px-5">
+        <nav className="container-fluid navbar bg-success navbar-expand-md fixed-top mb-5 color px-5">
   <a className="navbar-brand text-black" aria-current="page" href="">
     <img src="assets/Santa-Year-Round-Logo.png" height="70px" alt="logo" />
     <span className="text-danger" />
@@ -29,22 +28,22 @@ export default function Nav(){
   >
     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
       <li className="nav-item px-1">
-        <a className="nav-link active text-white" aria-current="page" href="">
+        <a className="nav-link active text-black" aria-current="page" href="">
           Home
         </a>
       </li>
       <li className="nav-item px-1">
-        <a className="nav-link  text-white" href="#services">
+        <a className="nav-link  text-black" href="#services">
           Services
         </a>
       </li>
       <li className="nav-item px-1 ">
-        <a className="nav-link text-white" href="#testimonials">
+        <a className="nav-link text-black" href="#testimonials">
           Testimonials
         </a>
       </li>
       <li className="nav-item px-1 ">
-        <a className="nav-link text-white" href="#contact">
+        <a className="nav-link text-black" href="#contact">
           Contact Us
         </a>
       </li>
