22b489ae79509c2a01dbe510454245d76689ead9
munyuawilson <wmunyua4@gmail.com>
2023-12-08 15:59:51 -0500

    login form

--- resources/js/components/loginform.js
+++ resources/js/components/loginform.js
@@ -7,11 +7,65 @@ export default function Login(){
 
     return (
         <div>
-            <form>
-                <div>
-                    
-                </div>
-            </form>
+            <div className="row custom-margin pt-5 mt-5">
+  <div className="col-md-5 mx-auto box-shadow ">
+    <h4 className="text-center">Log in</h4>
+    <div className="col-md-12">
+      <form role="form" action="login.php" method="POST">
+        <div className="form-group row">
+          <label
+            htmlFor="inputEmail3"
+            className="col-sm-3 col-form-label text-muted"
+          >
+            Email
+          </label>
+          <div className="col-sm-9">
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              id="inputEmail3"
+              placeholder="Email"
+              required=""
+            />
+          </div>
+        </div>
+        <div className="form-group row mt-3">
+          <label
+            htmlFor="inputPassword3"
+            className="col-sm-3 col-form-label text-muted"
+          >
+            Password
+          </label>
+          <div className="col-sm-9">
+            <input
+              type="password"
+              name="password"
+              className="form-control"
+              id="inputPassword3"
+              placeholder="Password"
+              required=""
+            />
+            <small>
+              <a href="forgot.php">Forgot Password</a>
+            </small>
+            <small>
+              <a href="create.php">Sign-Up</a>
+            </small>
+          </div>
+        </div>
+        <div className="form-group row">
+          <div className="pw">
+            <button type="submit" className="btn btn-primary">
+              Sign in
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
         </div>
     )
 }
