2d81abccbc51e576958dc614d6ee9f1df7eb85a5
munyuawilson <wmunyua4@gmail.com>
2023-12-10 21:15:41 -0500

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,7 +9,7 @@ import Testimonial from './components/testimonial';
 import Brand from './components/brand';
 import Secondsection from './components/secondsection';
 import LoginForm from './components/loginform';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
 //ReactDOm is not being used lately
 /*
