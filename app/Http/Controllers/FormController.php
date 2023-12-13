5f496160ef2aaa94305012abad712dd99227c3fe
munyuawilson <wmunyua4@gmail.com>
2023-12-12 23:59:35 -0500

    update

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class FormController extends Controller
 {
+    public function submitForm(Request $request)
+    {
+        // Handle form submission here
+        $name = $request->input('name');
+        echo $name;
+
+        // Perform authentication or any other processing
+
+        // Redirect or return a response
+        return redirect()->route('home');
+    }
     //
 }
